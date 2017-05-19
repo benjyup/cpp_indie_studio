26ddcc847ae93e2d199c1126eaac6cb56f632824
kyxo <florian.vincent@epitech.eu>
2017-05-19 17:27:57 +0200

    map

--- caca.cpp
+++ caca.cpp
@@ -2,34 +2,17 @@
 // Created by vincent on 05/05/17.
 //
 
+# include <map>
+# include <iostream>
 # include <irrlicht.h>
-
-/*
- * int 		main(void) {
-  is::GameEngine game("Indie Studio");
-  is::IGameState *gameState = new is::GameState;
-
-  // load the intro
-  // game.ChangeState( CIntroState::Instance() );
-
-  game.ChangeState(gameState);
-
-  // main loop
-  while (game.Running()) {
-     game.HandleEvents();
-    game.Update();
-    game.Draw();
-  }
-
-  // cleanup the engine
-  return 0;
-}
- */
+#include <vector>
 
 #ifdef _IRR_WINDOWS_
 #pragma comment(lib, "Irrlicht.lib")
 #pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 #endif
+
+#define SQUARE_SIZE 10
 
 using namespace irr;
 
@@ -45,48 +28,27 @@ int 		main()
   video::IVideoDriver* driver = device->getVideoDriver();
   scene::ISceneManager* smgr = device->getSceneManager();
   gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
+  std::vector<std::vector<scene::IAnimatedMeshSceneNode *>>			map;
+  video::ITexture 								*text[3];
 
+  text[0] = driver->getTexture("./gfx/groundGrass.png");
+  text[1] = driver->getTexture("./gfx/wallBrick.png");
+  text[2] = driver->getTexture("./gfx/wallStone.png");
   scene::IAnimatedMesh* mesh = smgr->getMesh("./gfx/wallBrick.obj");
-  scene::IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
-  if (node)
+  for (int i = 0; i < SQUARE_SIZE; i++)
     {
-      node->setMaterialFlag(video::EMF_LIGHTING, false);
-      node->setMD2Animation(scene::EMAT_STAND);
-      node->setMaterialTexture( 0, driver->getTexture("./gfx/groundGrass.png") );
-      node->setScale(core::vector3df(10,10,10));
-      node->setPosition(core::vector3df(0,0,0));
+      map.push_back(std::vector<scene::IAnimatedMeshSceneNode *>());
+      for (int j = 0; j < SQUARE_SIZE; j++)
+	{
+	  map[i].push_back(smgr->addAnimatedMeshSceneNode(mesh));
+	  map[i][j]->setMaterialFlag(video::EMF_LIGHTING, false);
+	  map[i][j]->setMD2Animation(scene::EMAT_STAND);
+	  map[i][j]->setMaterialTexture(0, text[(i + j) % 3]);
+	  map[i][j]->setScale(core::vector3df(10, 10, 10));
+	  map[i][j]->setPosition(core::vector3df(i * 10, j * 10, ));
+	  std::cout << "here" << i << std::endl;
+	}
     }
-  scene::IAnimatedMesh* mesh2 = smgr->getMesh("./gfx/wallBrick.obj");
-  scene::IAnimatedMeshSceneNode* node2 = smgr->addAnimatedMeshSceneNode( mesh2 );
-  if (node2)
-    {
-      node2->setMaterialFlag(video::EMF_LIGHTING, false);
-      node2->setMD2Animation(scene::EMAT_STAND);
-      node2->setMaterialTexture( 0, driver->getTexture("./gfx/groundGrass.png") );
-      node2->setScale(core::vector3df(10,10,10));
-      node2->setPosition(core::vector3df(0,0,10));
-    }
-  scene::IAnimatedMesh* mesh3 = smgr->getMesh("./gfx/wallBrick.obj");
-  scene::IAnimatedMeshSceneNode* node3 = smgr->addAnimatedMeshSceneNode( mesh3 );
-  if (node3)
-    {
-      node3->setMaterialFlag(video::EMF_LIGHTING, false);
-      node3->setMD2Animation(scene::EMAT_STAND);
-      node3->setMaterialTexture( 0, driver->getTexture("./gfx/groundGrass.png") );
-      node3->setScale(core::vector3df(10,10,10));
-      node3->setPosition(core::vector3df(10,0,0));
-    }
-  scene::IAnimatedMesh* mesh4 = smgr->getMesh("./gfx/wallBrick.obj");
-  scene::IAnimatedMeshSceneNode* node4 = smgr->addAnimatedMeshSceneNode( mesh4 );
-  if (node4)
-    {
-      node4->setMaterialFlag(video::EMF_LIGHTING, false);
-      node4->setMD2Animation(scene::EMAT_STAND);
-      node4->setMaterialTexture( 0, driver->getTexture("./gfx/groundGrass.png") );
-      node4->setScale(core::vector3df(10,10,10));
-      node4->setPosition(core::vector3df(10,0,10));
-    }
-
 
   scene::IAnimatedMesh* mesh5 = smgr->getMesh("./gfx/wallStone.obj");
   smgr->getMeshManipulator()->makePlanarTextureMapping(mesh5->getMesh(0), 0.004f);
@@ -112,9 +74,6 @@ int 		main()
   //smgr->addCameraSceneNodeFPS();
   //device->getCursorControl()->setVisible(false);
 
-
-  int lastFPS = -1;
-
   while(device->run())
     {
       if (device->isWindowActive())
@@ -122,22 +81,6 @@ int 		main()
 	  driver->beginScene(true, true, video::SColor(255,200,200,200));
 	  smgr->drawAll();
 	  driver->endScene();
-	  driver->beginScene(true, true, video::SColor(255,100,101,140));
-
-	  guienv->drawAll();
-
-	  int fps = driver->getFPS();
-
-	  if (lastFPS != fps)
-	    {
-	      core::stringw str = L"Irrlicht Engine - Quake 3 Map example [";
-	      str += driver->getName();
-	      str += "] FPS:";
-	      str += fps;
-
-	      device->setWindowCaption(str.c_str());
-	      lastFPS = fps;
-	    }
 	}
       else
 	device->yield();
