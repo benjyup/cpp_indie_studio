70ac8e090c531364218e92c23f517fa60197beb0
peixot_b <benjamin.peixoto@epitech.eu>
2017-05-24 18:30:45 +0200

    Ajout de la classe planetSystem (permettant de faire tournoyer des planete a disance de la map

--- map/benjo/srcs/main.cpp
+++ map/benjo/srcs/main.cpp
@@ -3,6 +3,7 @@
 #include "MapIndie.hpp"
 #include "ObjectMap.hpp"
 #include "Fire.hpp"
+#include "PlanetSystem.hpp"
 #include "IObject.hpp"
 #include <irrlicht.h>
 
@@ -38,30 +39,7 @@ int	main()
 
 
   // SOLEIL GRAVITANT
-  irr::scene::IAnimatedMesh* meshParticule = smgr->getMesh("./gfx/wallBrick.obj");
-  smgr->getMeshManipulator()->makePlanarTextureMapping(meshParticule->getMesh(0), 0.004f);
-
-  irr::scene::ISceneNode* node = 0;
-
-  node = smgr->addAnimatedMeshSceneNode(meshParticule);
-  node->setMaterialTexture(0, driver->getTexture("./gfx/nothing.png"));
-  node->getMaterial(0).SpecularColor.set(100,100,100,0);
-
-  node = smgr->addLightSceneNode(0, irr::core::vector3df(50,50,50),
-				 irr::video::SColorf(1.0f, 0.6f, 0.7f, 1.0f), 800.0f);
-  irr::scene::ISceneNodeAnimator* anim = 0;
-  anim = smgr->createFlyCircleAnimator(irr::core::vector3df(0,150,0),400.0f);
-  node->addAnimator(anim);
-  anim->drop();
-
-  // attache le panneau à la lumière
-
-  node = smgr->addBillboardSceneNode(node, irr::core::dimension2d<irr::f32>(50, 50));
-  node->setPosition(irr::core::vector3df(0, -100, 0));
-  node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
-  node->setMaterialType(irr::video::EMT_TRANSPARENT_ADD_COLOR);
-  node->setMaterialTexture(0, driver->getTexture("./gfx/particlered.bmp"));
-
+  PlanetSystem	Planet(smgr, driver, "./gfx/particlered.bmp", 800.0f, 0.01f);
 
 
   smgr->addCameraSceneNodeFPS();
