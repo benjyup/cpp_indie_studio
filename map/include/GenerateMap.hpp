d279a9176b4e7b75ba330a2ed6a0b8cb7d97b6b9
peixot_b <benjamin.peixoto@epitech.eu>
2017-06-15 16:57:24 +0200

    Generate map class

---
+++ map/include/GenerateMap.hpp
@@ -0,0 +1,15 @@
+//
+// Created by peixot_b on 15/06/17.
+//
+
+#ifndef MAP_GENERATEMAP_HPP
+#define MAP_GENERATEMAP_HPP
+
+
+class GenerateMap
+{
+
+};
+
+
+#endif //MAP_GENERATEMAP_HPP
---
+++ map/srcs/GenerateMap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by peixot_b on 15/06/17.
+//
+
+#include "GenerateMap.hpp"
