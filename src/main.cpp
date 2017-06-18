f21abc362e3b8d520d524834b8cc2b107ba7af5c
gregoire renard <gregoire.renard@epitech.eu>
2017-06-18 23:33:36 +0200

    fullscreen

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int 		main(void) {
 
   try {
       std::shared_ptr<is::GameEngine> engine = std::make_shared<is::GameEngine>("Indie Studio",
-										1900, 1000, 0, false);
+										1900, 1000, 0, true);
       engine->PushState(new is::IntroState);
 
       irr::core::array<irr::SJoystickInfo> joystickInfo;
